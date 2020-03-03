dd3d082d96e86ff5c81781eb868976ee3a16b01c
Patrick <thuuor@gmail.com>
2020-03-03 15:29:23 +0100

    Added ScoreBoard

---
+++ src/game/ScoreBoardIncrementer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+/**
+ *
+ * @author 10229590
+ */
+public interface ScoreBoardIncrementer {
+    public void incrementScore(int increment);
+}
